1dd170bcca560dfcd9580bdd6c6ae4e49c8fbcf9
BlackWind001 <msanirudh1998@gmail.com>
2021-10-16 14:23:44 +0530

    Created separate bundle for print.js and used HTMLWebpackPlugin to create a different index.html from before.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
+import printMe from './print';
 // run `node index.js` in the terminal
 
 // console.log(`Hello Node.js v${process.versions.node}!`);
 
-window.onload = () => {
-  const h1 = document.createElement('h1');
-  h1.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  document.appendChild(h1);
-};
+const h1 = document.createElement('h1');
+h1.innerHTML = _.join(['Hello', 'webpack'], ' ');
+document.body.appendChild(h1);
+printMe('I am data');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'none',
+  entry: {
+    index: './src/index.js',
+    printMe: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  }
 };
