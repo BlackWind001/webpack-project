4180aef96269d1820e40f29c2b5ad26eb84b8424
BlackWind001 <msanirudh1998@gmail.com>
2021-10-17 06:32:42 +0530

    Experimented with a webpack-deadcode-plugin to help remove dead code and unused files from the project.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # node-vqsfvb
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-vqsfvb)
+
+
+Added a dev dependency called `webpack-deadcode-plugin` that will help identify unused files.
+Also added a script to utilize the plugin.
+The implementation is similar to the plugin used in this (video)[https://egghead.io/lessons/webpack-delete-unused-code-with-webpack-and-unused-files-webpack-plugin] which is not supported on Webpack 5.
+Hence, the `webpack-deadcode-plugin` is being used as an alternative.
+The inspiration to find out about unused code in project comes from this stackoverflow question that I can relate to: https://stackoverflow.com/questions/69594518/how-to-find-unused-images-in-public-folder-in-react-app
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DeadCodePlugin = require('webpack-deadcode-plugin');
 const path = require('path');
 
-module.exports = {
-  mode: 'none',
-  entry: {
-    printMe: './src/print.js',
-    index: './src/index.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist'
+module.exports = (env) => {
+  let config = {
+    mode: 'none',
+    entry: {
+      printMe: './src/print.js',
+      index: './src/index.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Output Management'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        }
+      ]
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist'
+    }
+  };
+
+  if (env && env.checkUnused) {
+    config.plugins.push(
+      new DeadCodePlugin({
+        patterns: ['src/**/*.(js|jsx|css|png|jpg|jpeg)']
+      })
+    );
   }
-};
+
+  return config;
+}
